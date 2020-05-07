a9ea4dc43aaf9e48b6e3859dc25abf38e493b048
Daniela Diaz Bejarano <Daniela.diaz_bej@uao.edu.co>
2020-05-07 16:09:08 -0500

    C Bt Reservar
    Modal en boton de reserva de salones e imagener en el home

--- uaoreservas/src/Components/Button/Button.js
+++ uaoreservas/src/Components/Button/Button.js
@@ -15,7 +15,27 @@ export default function ContainedButtons() {
 
   return (
     <div className={classes.root} className="o-container-btnReservar">
-      <Button variant="contained" color="primary" className="o-btnReservar">Reservar</Button>
+      <Button variant="contained" color="primary" data-toggle="modal" data-target="#ResAlert"  className="o-btnReservar">Reservar</Button>
+
+      
+<div class="modal fade" id="ResAlert" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Reservado</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+        El salon seleccionado ha sido reservado
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Aceptar</button>
+        </div>
+    </div>
+</div>
+</div>
     </div>
   );
 }
--- uaoreservas/src/Components/EventsG/EventsG.js
+++ uaoreservas/src/Components/EventsG/EventsG.js
@@ -8,27 +8,30 @@ class EventsG extends React.Component {
         return (
 <div class="card-deck">
   <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
+    <img src="https://scontent.fclo5-1.fna.fbcdn.net/v/t1.0-9/p960x960/96161017_3384189754958594_5963776774436290560_o.jpg?_nc_cat=109&_nc_sid=8024bb&_nc_eui2=AeHyymYSZ6vrFVS5qXw54zW9uLqwcwqu6Ga4urBzCq7oZlFZZMKbBaiEW-WaratzSu6pEhAu7rG1jAA_3WHwew9H&_nc_oc=AQlWHqJlqFzHnGYFxeRdSZ1_4yth6pcu55Z-I2QbsZ3Ayp9rxX5kn-3Teki9RxGvHEs&_nc_ht=scontent.fclo5-1.fna&_nc_tp=6&oh=d937e52a0a0279fc4c6cc6994083ad26&oe=5ED8E426" class="card-img-top" alt="Invitacion a webinar"/>
     <div class="card-body">
-      <h5 class="card-title">Evento 1</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">Webinar</h5>
+      <p class="card-text ">El mercado laboral está evolucionando y demandando nuevas profesiones. ¿Qué es y cuáles son las características de un buen 'networker'? Descúbrelo en este webinar gratuito.
+
+Inscríbete aquí: https://bit.ly/webinars-gratuitosUAO</p>
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
+    <img src="https://scontent.fclo5-1.fna.fbcdn.net/v/t1.0-9/p960x960/96215604_3377624232281813_83404158899585024_o.png?_nc_cat=111&_nc_sid=8024bb&_nc_eui2=AeFgXsfz4uTvLT6JAmGy-dT3v11a97T-mri_XVr3tP6auCzQorTgBfmLhX2UsCOGLe46QKPCAGCNqlx8ZatwgvNb&_nc_oc=AQm7hjv2Wzbfpgl7bxLwigPdVREIOqdno1Il8gY-aLpJTqHcyW87uc5WNsnh-qXOnv0&_nc_ht=scontent.fclo5-1.fna&oh=5a9742f5a99c4c3dd0daff9942f838b0&oe=5ED967FB" class="card-img-top" alt="Invitacion a Webinar"/>
     <div class="card-body">
-      <h5 class="card-title">Evento 2</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">Webinar</h5>
+      <p class="card-text">¿Por qué es clave la gestión del conocimiento y la innovación en las empresas, especialmente en entornos de riesgo? De esto hablará Karim Hussein Dasuki, miembro de un equipo de docentes investigadores de Colombia y México que adelanta un proyecto de interacción con pequeñas y medianas empresas. Participa y obtén un certificado de asistencia. Inscríbete en: https://bit.ly/3dk5HD1
+</p>
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
+    <img src="https://scontent.fclo5-1.fna.fbcdn.net/v/t1.0-9/p960x960/95608397_3373063759404527_8189013976332369920_o.jpg?_nc_cat=108&_nc_sid=8024bb&_nc_eui2=AeGOj8AyoMxNuVeome7rt_T6e4dyVdjHxXl7h3JV2MfFeeiN80nJxKy0M5s5UW9RLuAsInogbWInvbwiN3pqYhq0&_nc_oc=AQneOMVYQnFr_74KcCLQcvZwDDYl3ljGH9lqcUcZve82tfjKQ4OIvzPikKZxXKsG_4o&_nc_ht=scontent.fclo5-1.fna&_nc_tp=6&oh=31ae902b49acaca05fa71cf1f19d0cd6&oe=5ED841C1" class="card-img-top" alt="Invitacion a Webinar"/>
     <div class="card-body">
-      <h5 class="card-title">Evento 3</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">Webinar</h5>
+      <p class="card-text">Descubre en este webinar gratuito cómo adelantarte a la crisis y reinventarte en tiempos de incertidumbre, generando ideas con alto potencial innovador. ¡Te esperamos!
+
+Inscríbete aquí: https://bit.ly/webinars-gratuitosUAO
+</p>
     </div>
   </div>
 </div>
